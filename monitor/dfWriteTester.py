cb891d844088ac7de27f071f21156011823d94d5
ceky <wpw436@gmail.com>
2020-11-27 18:34:36 -0800

    add fb historical data example

--- monitor/dfWriteTester.py
+++ monitor/dfWriteTester.py
@@ -2,8 +2,8 @@ from dfWriterBase import *
 from historical.wallstreet import Stock, Call, Put
 
 dfb = dfWriterBase()
-path='../datahut/data/historical/FB/'
-s = Stock('FB')
+path='../datahut/data/historical/fb/'
+s = Stock('fb')
 df = s.historical(days_back=30000, frequency='d')
 
 dfb.writeDfTo(path, df)
