21f1dc12aa8b6db742a50148c085ef4a00898535
SakuraCoder <632588258@qq.com>
2020-11-29 22:37:51 -0800

    add data loader for daily data

--- monitor/dfWriteTester.py
+++ monitor/dfWriteTester.py
@@ -1,9 +1,13 @@
 from dfWriterBase import *
 from historical.wallstreet import Stock, Call, Put
+import yfinance as yf
+
+# dfb = dfWriterBase()
+# path='../datahut/data/historical/fb/'
+# s = Stock('fb')
+# df = s.historical(days_back=30000, frequency='d')
 
 dfb = dfWriterBase()
-path='../datahut/data/historical/fb/'
-s = Stock('fb')
-df = s.historical(days_back=30000, frequency='d')
-
-dfb.writeDfTo(path, df)
+path='../datahut/data/daily/fb/'
+df = yf.download('fb', period='1d', interval='1m')
+dfb.writeDailyDfTo(path, df)
--- monitor/dfWriterBase.py
+++ monitor/dfWriterBase.py
@@ -4,7 +4,7 @@ from os import path, makedirs
 parse_dates = ['Date']
 
 class dfWriterBase:
-  def writeDfTo(self, base_path='', data_frame=pd.DataFrame()):
+  def writeHistoricalDfTo(self, base_path='', data_frame=pd.DataFrame()):
     lines_num = data_frame.shape[0]
 
     if lines_num <= 0 or base_path == '':
@@ -31,6 +31,70 @@ class dfWriterBase:
 
         last_index = i
         last_filename = curr_filename
+
+  def writeDailyDfTo(self, base_path='', data_frame=pd.DataFrame()):
+    lines_num = data_frame.shape[0]
+
+    if lines_num <= 0 or base_path == '':
+      print('empty data_frame/base_path input! return..')
+      return
+
+    if not path.exists(base_path):
+      makedirs(base_path)
+
+    last_filename = data_frame.index[0].strftime('%Y%m%d')
+    last_index = 0
+    for i in range(1, lines_num):
+      curr_filename = data_frame.index[i].strftime('%Y%m%d')
+      if last_filename != curr_filename or i >= lines_num - 1:
+        filepath = base_path + last_filename + '.csv'
+        end_index = i
+        date_last_index = data_frame.index[last_index]
+        date_end_index = data_frame.index[end_index]
+        if not path.exists(filepath):
+          file_ptr = open(filepath, "w")
+          
+          data_frame[date_last_index: date_end_index].to_csv(path_or_buf=file_ptr, date_format='%Y-%m-%d-%H-%M-%S')
+          file_ptr.close()
+        else:
+          self.appendDfToFullPathDaily(filepath, data_frame[date_last_index: date_end_index])
+
+        last_index = i
+        last_filename = curr_filename
+
+  def appendDfToFullPathDaily(self, full_path='', data_frame=pd.DataFrame()):
+    if data_frame.shape[0] <= 0 or full_path == '':
+      print('empty data_frame/full_path input! return..')
+      return
+
+    date_parser = lambda col: pd.to_datetime(col, format="%Y-%m-%d-%H-%M-%S")
+    old_content = pd.read_csv(full_path, index_col=0, date_parser=date_parser)
+
+    print (data_frame.index)
+    
+    result = pd.concat([old_content, data_frame])
+
+    f = open("test1.csv", "w")
+    result.to_csv(path_or_buf=f, date_format='%Y-%m-%d-%H-%M-%S')
+    f.close()
+    
+    # # sorting by Date
+    result.sort_index()
+
+    # f = open("test1.csv", "w")
+    # result.to_csv(path_or_buf=f)
+    # f.close()
+      
+    # dropping ALL duplicte values
+    result.drop_duplicates(subset ="index")
+    
+    old_num = old_content.shape[0]
+    new_num = result.shape[0]
+
+    if new_num > old_num:
+      file_ptr = open(full_path, "w")
+      result.to_csv(index=False, path_or_buf=file_ptr)
+      file_ptr.close()
 
   def appendDfToFullPath(self, full_path='', data_frame=pd.DataFrame()):
     if data_frame.shape[0] <= 0 or full_path == '':
