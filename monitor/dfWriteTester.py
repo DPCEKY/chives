ef972c8507219c9fb306d343eff64406b0d356d8
ceky <wpw436@gmail.com>
2020-12-26 15:06:15 -0800

    testing daily monitor

--- monitor/dfWriteTester.py
+++ monitor/dfWriteTester.py
@@ -4,12 +4,12 @@ from historical.wallstreet import Stock, Call, Put
 import yfinance as yf
 
 dfb = dfWriterBase()
-path='../datahut/data/historical/goog/'
+path = '../datahut/data/historical/goog/'
 s = Stock('goog')
 df = s.historical(days_back=40, frequency='d')
 dfb.writeDfTo(path, df)
 
 dfb = dfWriterDaily()
-path='../datahut/data/daily/fb/'
-df = yf.download('fb', period='5d', interval='1m')
+path = '../datahut/data/daily/fb/'
+df = yf.download('fb', period='1d', interval='1m', prepost=True)
 dfb.writeDfTo(path, df)
